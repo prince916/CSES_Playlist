4152cd42040ac2e1e92b14250e07f483394979bc
Your Name <kumarprince08081999@gmail.com>
2026-09-17 17:56:15 +0530

    new commitn

---
+++ temp2.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+// int main() 
+// {
+//     int a=60,b=5;
+//     printf("sum=%d\n",a+b);
+//     printf("difference=%d\n",a-b);
+//     printf("product=%d\n",a*b);
+//     printf("quotient=%d\n",a/b);
+//     printf("Remainder=%d\n",a%b);
+//     return 0;
+// }
+
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a=6,b=5,c;
+//     c=a+b;
+//     printf("sum=%d\n",c);
+//     return 0;
+// }
+
+
