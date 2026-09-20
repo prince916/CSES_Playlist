4ca142f98791fd793c200752b4cde95089dfc18e
Your Name <kumarprince08081999@gmail.com>
2026-09-20 12:55:47 +0530

    new problems

---
+++ TraillingZeros.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    long long count = 0;
+
+    while (n > 0) {
+        n /= 5;
+        count += n;
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
